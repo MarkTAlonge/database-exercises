2113541b5f8f52d065c417aa0a2a3a8c253d336b
Mark Alonge <Mark.T.Alonge@gmail.com>
2020-07-07 23:32:58 -0500

    exercise for tablesm and databases and basic statements, and sample db and some intellij setup

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,12 +1,15 @@
-USE codeup_test_db,
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
 
 CREATE TABLE IF NOT EXISTS albums (
-    id INT UNSIGNED AUTO_INCREMENT,
-         artist VARCHAR(100),
+    id float UNSIGNED AUTO_INCREMENT,
+         artist VARCHAR(100) default 'unknown',
          name VARCHAR(100),
-         release_date DATE,
-         sales float,
+         release_date smallint,
          genre TEXT NOT NULL,
+         sales float,
          PRIMARY KEY(id)
 
 )
